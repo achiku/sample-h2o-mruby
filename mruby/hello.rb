6fd2434fb6434e67742e0be6530dd25e8d3885df
Akira Chiku <akira.chiku@gmail.com>
2015-10-10 21:11:35 +0900

    Fix format

--- mruby/hello.rb
+++ mruby/hello.rb
@@ -1,30 +1,28 @@
 # paths:
 #   /:
-#     file.dir: examples/doc_root
 #     mruby.handler-file: /path/to/hello.rb
 
 class HelloApp
+  # HelloApp Class
   def call(env)
-    h = "hello"
-    m = "from h2o_mruby"
-
-    ua = env["HTTP_USER_AGENT"]
+    h = 'hello'
+    m = 'from h2o_mruby'
+    ua = env['HTTP_USER_AGENT']
     new_ua = "new-#{ua}-h2o_mruby"
-    path = env["PATH_INFO"]
-    host = env["HTTP_HOST"]
-    method = env["REQUEST_METHOD"]
-    query = env["QUERY_STRING"]
-
-    msg = "#{h} #{m}. User-Agent:#{ua} New User-Agent:#{new_ua} path:#{path} host:#{host} method:#{method} query:#{query}"
-
-    [200,
-     {
-       "content-type" => "text/plain; charset=utf-8",
-       "user-agent" => new_ua,
-     },
-     ["#{msg}\n"]
+    path = env['PATH_INFO']
+    host = env['HTTP_HOST']
+    method = env['REQUEST_METHOD']
+    query = env['QUERY_STRING']
+    msg = "#{h} #{m}. User-Agent:#{ua} New User-Agent:#{new_ua}"
+    msg += "path:#{path} host:#{host} method:#{method} query:#{query}"
+    [
+      200,
+      {
+        'content-type' => 'text/plain; charset=utf-8',
+        'user-agent' => new_ua
+      },
+      ["#{msg}\n"]
     ]
-
   end
 end
 
